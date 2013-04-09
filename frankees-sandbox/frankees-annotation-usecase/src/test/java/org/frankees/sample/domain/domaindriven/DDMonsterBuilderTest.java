ddfb351b833d31a2dc2f5bab9b02b23a17f58d04
Laurent <laurent.ruaud@serli.com>
2013-04-09 23:49:20 +0200

    Package renaming for consistency.

---
+++ frankees-sandbox/frankees-annotation-usecase/src/test/java/org/frankees/sample/domain/domaindriven/DDMonsterBuilderTest.java
@@ -0,0 +1,18 @@
+package org.frankees.sample.domain.domaindriven;
+
+import static org.fest.assertions.Assertions.*;
+
+import org.junit.Test;
+
+public class DDMonsterBuilderTest {
+
+	@Test
+	public void testBuilder() {
+		DDMonster dto = DDMonsterBuilder.aDDMonster().withName("Yeti").build();
+		
+		assertThat(dto).isNotNull();
+		assertThat(dto.getName()).isNotNull();
+		assertThat(dto.getName()).isEqualTo("Yeti");
+	}
+	
+}

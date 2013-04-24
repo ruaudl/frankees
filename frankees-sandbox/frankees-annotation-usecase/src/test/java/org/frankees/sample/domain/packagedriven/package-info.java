faf372d27b1759ca23c3582a13402faddc300737
Laurent <laurent.ruaud@serli.com>
2013-04-25 00:52:44 +0200

    Restoring tests on package driven annotation processing using inner annotations.

--- frankees-sandbox/frankees-annotation-usecase/src/test/java/org/frankees/sample/domain/packagedriven/package-info.java
+++ frankees-sandbox/frankees-annotation-usecase/src/test/java/org/frankees/sample/domain/packagedriven/package-info.java
@@ -1,5 +1,7 @@
-@DataBuilders(builderClassSuffix = "Assembler", builderPackageName = "org.frankees.sample.domain.packagedriven.test", beanClassName= "(?:(?!Test)\\w)+", beanPackageName = "org.frankees.sample.domain.packagedriven")
+@DataBuilders(builderClassSuffix = "Assembler", builderPackageName = "org.frankees.sample.domain.packagedriven.test", value = {
+		@DataBuilder(PDMonster.class), @DataBuilder(PDCharacter.class) })
 package org.frankees.sample.domain.packagedriven;
 
+import org.frankees.annotation.DataBuilder;
 import org.frankees.annotation.DataBuilders;
 
